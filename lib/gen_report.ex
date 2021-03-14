729f7239a83af569eab37e6905112cb9ea17f5ba
LuizFerK <luizfernandoklein@live.com>
2021-03-14 15:32:12 -0300

    init: Start from non parallel project

---
+++ lib/gen_report.ex
@@ -0,0 +1,83 @@
+defmodule GenReport do
+  alias GenReport.Parser
+
+  @names [
+    "daniele",
+    "mayk",
+    "giuliano",
+    "cleiton",
+    "jakeliny",
+    "joseph",
+    "diego",
+    "rafael",
+    "vinicius",
+    "danilo"
+  ]
+
+  @months [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+  ]
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(hours_acc(), fn row, acc ->
+      get_hours(row, acc)
+    end)
+  end
+
+  defp get_hours([name, hours, _day, month, year], %{
+         "all_hours" => all_hours,
+         "hours_per_month" => hours_per_month,
+         "hours_per_year" => hours_per_year
+       }) do
+    all_hours = Map.put(all_hours, name, all_hours[name] + hours)
+
+    hours_per_month =
+      Map.put(
+        hours_per_month,
+        name,
+        Map.put(
+          hours_per_month[name],
+          month,
+          hours_per_month[name][month] + hours
+        )
+      )
+
+    hours_per_year =
+      Map.put(
+        hours_per_year,
+        name,
+        Map.put(hours_per_year[name], year, hours_per_year[name][year] + hours)
+      )
+
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
+  end
+
+  defp hours_acc do
+    month_list = Enum.into(@months, %{}, &{&1, 0})
+    year_list = Enum.into(2016..2020, %{}, &{&1, 0})
+
+    %{"all_hours" => %{}, "hours_per_month" => %{}, "hours_per_year" => %{}}
+    |> Map.put("all_hours", acc_name_map_gen(0))
+    |> Map.put("hours_per_month", acc_name_map_gen(month_list))
+    |> Map.put("hours_per_year", acc_name_map_gen(year_list))
+  end
+
+  defp acc_name_map_gen(value), do: Enum.into(@names, %{}, &{&1, value})
+end
